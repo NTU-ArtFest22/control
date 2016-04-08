a5ac2eb0eb7f900cf4b241a53f7c6896178cb8cc
Amy Lin <linamy85@gmail.com>
2016-04-08 23:28:47 +0800

    fix async in getting act detail

--- public/javascripts/ng-profile.js
+++ public/javascripts/ng-profile.js
@@ -18,21 +18,28 @@
     };
 
     var rundetail = function(){
-      for( var i = 0 ; i < $scope.user.activities.length ; i++ ){
-        for( var act in $scope.activitylist ){
-          if( act._id == $scope.user.activities[i].id ){
-            $scope.user.activities[i].location = act.location;
-            $scope.user.activities[i].time = act.time;
-            $scope.user.activities[i].isRunning = act.isRunning;
-            console.log( $scope.user.activities[i] );
-            break;
+      $http.get('/user').success(function(response) {
+        $scope.user = response;
+        $scope.ctrl = "";
+        $http.get('/activitylist').success(function(res){
+          $scope.activitylist = res;
+
+          for( var i = 0 ; i < $scope.user.activities.length ; i++ ){
+            for( var act in $scope.activitylist ){
+              if( act._id == $scope.user.activities[i].id ){
+                $scope.user.activities[i].location = act.location;
+                $scope.user.activities[i].time = act.time;
+                $scope.user.activities[i].isRunning = act.isRunning;
+                console.log( $scope.user.activities[i] );
+                break;
+              }
+            }
           }
-        }
-      }
+        });
+
+      });
     };
 
-    refreshUser();
-    refreshList();
     rundetail();
 
     $scope.addActivity = function( uid , acid ){
