49dc4e30d84aa4a712f5117011baa33ade393e5f
Amy Lin <linamy85@gmail.com>
2016-04-21 22:00:44 +0800

    fix countdown bug

--- public/javascripts/ng-stream.js
+++ public/javascripts/ng-stream.js
@@ -181,7 +181,7 @@
       rtc.loadData();
       rtc.reloadGroup();
       $scope.countTime = 0;
-      $timeout($scope.onTimeout, 1000);
+      mytimeout = $timeout($scope.onTimeout, 1000);
     }
 
     //initial load
@@ -206,20 +206,13 @@
 
     window.initMap = function() {
       var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 3,
-        center: {lat: 0, lng: -180},
+        zoom: 7,
+        center: {lat:  25.038085 , lng:121.538231},
         mapTypeId: google.maps.MapTypeId.TERRAIN
       });
 
-      var flightPlanCoordinates = [
-        {lat: 37.772, lng: -122.214},
-        {lat: 21.291, lng: -157.821},
-        {lat: -18.142, lng: 178.431},
-        {lat: -27.467, lng: 153.027}
-      ];
       var flightPath = new google.maps.Polyline({
-        path: flightPlanCoordinates,
-        geodesic: true,
+        //geodesic: true,
         strokeColor: '#FF0000',
         strokeOpacity: 1.0,
         strokeWeight: 2
