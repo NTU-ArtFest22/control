08b1149501c69045c4269be9d5f100290f15182e
turing <kevin831130810620@gmail.com>
2016-05-05 03:29:05 +0800

    only query in act

--- routes/index.js
+++ routes/index.js
@@ -91,12 +91,14 @@ module.exports = function(passport, streams){
   });
   router.get('/api/act/get_all/:access_id', function(req,res){
     var access_id = req.params.access_id;
-    return User
-    .findOne({"fb.id":access_id}, function(err, user){
-      console.log("========");
-      console.log(user._id.toString()+' '+user.fb.displayName);
-      console.log(JSON.stringify(user, 4 , ''))
-      return Activity.find(
+    // return User
+    // .findOne({"fb.id":access_id}, function(err, user){
+    //   console.log("========");
+    //   console.log(user._id.toString()+' '+user.fb.displayName);
+    //   console.log(JSON.stringify(user, 4 , ''))
+    //   return 
+    // })
+    return Activity.find(
               {
                 "group.artist.id": mongojs.ObjectId("56fec493b158b4162f8ef380"),
                 "isRunning": true
@@ -110,7 +112,6 @@ module.exports = function(passport, streams){
                   return res.json(act)
                 }
               })
-    })
   });
   router.get('/api/act/get_act_info/:act_id', function(req,res){
     var act_id = req.params.act_id;
