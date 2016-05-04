7f8a295429dc9c1b08c41ad2c142a1c603922cb8
turing <kevin831130810620@gmail.com>
2016-05-05 03:37:05 +0800

    use elemMatch

--- routes/index.js
+++ routes/index.js
@@ -100,7 +100,11 @@ module.exports = function(passport, streams){
     // })
     return Activity.find(
               {
-                // "group.artist.id": mongojs.ObjectId("56fec493b158b4162f8ef380"),
+                "group":{
+                  $elemMatch: {
+                    "artist.id": mongojs.ObjectId("56fec493b158b4162f8ef380"),
+                  }
+                }
                 "isRunning": true
               }, 
               function(err, act){
