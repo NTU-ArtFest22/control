58af0abe3230b6d496cc3a23f41683d9002f1d50
turing <kevin831130810620@gmail.com>
2016-05-06 02:36:31 +0800

    query bug fix

--- routes/data.js
+++ routes/data.js
@@ -981,7 +981,7 @@ module.exports = function( app , db ){
     exchange_status: function(ex_data, callback){
       console.log("start exchange in data");
       Activity.find({ 
-          $and: [{"_id": mongojs.ObjectId(ex_data.act_id)}, {$or:[{"character":ex_data.self_character}, {"character":ex_data.other_character}]}]
+          $and: [{"_id": mongojs.ObjectId(ex_data.act_id)}, {$or:[{"group.character":ex_data.self_character}, {"group.character":ex_data.other_character}]}]
         }, {'group.$':1, isRunning: 1}, 
         function(err, doc){
         if(err){
